b84bc6c4bc9ea9372a081f1160e162c496eafe15
Alex Diaz <Alex Diaz@microsof-169fb9>
2014-03-26 13:45:19 +0300

    Лаба 10 готова

--- Exercise10/src/com/acme/Bank.java
+++ Exercise10/src/com/acme/Bank.java
@@ -6,35 +6,37 @@ public class Bank implements Serializable {
 
 	private final Client[] clients = new Client[10];
 	private int maxClients = 0;
-	private ClientRegistrationListener[] listeners;
+	private ClientRegistrationListener[] listeners = null;
 
-	public Bank() {
+	public Bank(ClientRegistrationListener[] listeners) {
 		
-		listeners = new ClientRegistrationListener[2];
-		// создаем слушатели событий
-		listeners[0] = new PrintClientListener();
-		listeners[1] = new EmailNotificationListener();
+		this.listeners = listeners;
 	}
 
 	public Client addClient(final Client client) {
 		clients[maxClients++] = client;
 
 		// TODO: iterate a list of listeners and notify about new client
+		for ( ClientRegistrationListener listener : listeners ) {
+			// вызываем метод события
+			listener.onClientAdded(client);
+		}  
+		
 		return client;
 	}
 	
 	
 	// ----------------------------INNER CLASSES-----------------------------
-	protected static class EmailNotificationListener
+	public static class EmailNotificationListener
 	implements ClientRegistrationListener {
 		
 		public void onClientAdded(Client c) {
 			System.out.println("Notification email for client "
-					+ c.getName() + " to be sent.");
+					+ c.getSalutation() + " to be sent.");
 		}
 	}
 	
-	protected static class PrintClientListener 
+	public static class PrintClientListener 
 	implements ClientRegistrationListener {
 		
 		public void onClientAdded(Client c) {
--- Exercise10/src/com/acme/BankApplication.java
+++ Exercise10/src/com/acme/BankApplication.java
@@ -2,6 +2,25 @@ package com.acme;
 
 public class BankApplication {
 	public static void main(String[] args) {
-		System.out.println("Hello,Alex!");
+		
+		// создаем клиентов
+		Client clients[] = new Client[4];
+		clients[0] = new Client("Andersen",	Gender.MALE);
+		clients[1] = new Client("Smith", 	Gender.MALE);
+		clients[2] = new Client("Holmes",	Gender.MALE);
+		clients[3] = new Client("Watson", 	Gender.MALE);
+		
+		// слушатели событий
+		ClientRegistrationListener[] listeners = new ClientRegistrationListener[2]; 
+		listeners[0] = new Bank.PrintClientListener();
+		listeners[1] = new Bank.EmailNotificationListener();
+		
+		// создаем банк
+		Bank bank = new Bank(listeners);
+		
+		// добавление клиентов в банк
+		for ( Client client : clients ) {
+			bank.addClient(client);
+		}
 	}
 }
