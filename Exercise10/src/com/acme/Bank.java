7085624d5c3ead37294ba818178b685cf4b490d5
Alex Diaz <Alex Diaz@microsof-169fb9>
2014-03-26 13:19:01 +0300

    Добавлены вложенные классы PrintClientListener EmailNotificationListener

--- Exercise10/src/com/acme/Bank.java
+++ Exercise10/src/com/acme/Bank.java
@@ -9,7 +9,11 @@ public class Bank implements Serializable {
 	private ClientRegistrationListener[] listeners;
 
 	public Bank() {
-
+		
+		listeners = new ClientRegistrationListener[2];
+		// создаем слушатели событий
+		listeners[0] = new PrintClientListener();
+		listeners[1] = new EmailNotificationListener();
 	}
 
 	public Client addClient(final Client client) {
@@ -17,6 +21,25 @@ public class Bank implements Serializable {
 
 		// TODO: iterate a list of listeners and notify about new client
 		return client;
+	}
+	
+	
+	// ----------------------------INNER CLASSES-----------------------------
+	protected static class EmailNotificationListener
+	implements ClientRegistrationListener {
+		
+		public void onClientAdded(Client c) {
+			System.out.println("Notification email for client "
+					+ c.getName() + " to be sent.");
+		}
+	}
+	
+	protected static class PrintClientListener 
+	implements ClientRegistrationListener {
+		
+		public void onClientAdded(Client c) {
+			System.out.println(c.toString());
+		}
 	}
 
 }
--- Exercise10/src/com/acme/ClientRegistrationListener.java
+++ Exercise10/src/com/acme/ClientRegistrationListener.java
@@ -1,6 +1,5 @@
 package com.acme;
 
-
 public interface ClientRegistrationListener {
 	public void onClientAdded(Client client);
 }
